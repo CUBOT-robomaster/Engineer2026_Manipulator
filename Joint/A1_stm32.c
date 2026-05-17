0718118a942bd7fc5cdc9abfcf349faea21e26ec
Administrater <xikangjiao@gmail.com>
2026-05-17 13:58:44 +0800

    first commit

--- Joint/A1_stm32.c
+++
@@ -1,9 +0,0 @@
-#include "A1_stm32.h"
-#include "hardware_config.h"
-#include "control_logic.h"
-#include <math.h>
-#include <string.h>
-#include "joint_control.h"
-#include "attitude.h"
-
-#define abs(x) ((x>=0)?(x):(-x))
